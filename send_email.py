812b791e60bbb57d29ac31d6b02de56a1c36be07
Vikasrao1 <tvikasrao1@gmail.com>
2025-02-18 20:09:29 -0600

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ import os
 port = 465
 smtp_server = "smtp.gmail.com"
 
-USERNAME = os.environ.get('USER_EMAIL')   # instead of giving our email we're utilizing env varibale    to secure the creds
+USERNAME = os.environ.get('USER_EMAIL')   # instead of giving our email we're utilizing env varibale to secure the creds
 PASSWORD = os.environ.get('USER_PASSWORD')
 message = """\
 Subject: Github Email report
@@ -12,5 +12,5 @@ This is your email notification"""
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-  server.login(USERNAME,PASSWORD) # we're logging into smtp server
-  server.sendmail(USERNAME,USERNAME, message) # we're sending it to ourself
+  server.login(USERNAME,PASSWORD) # logging into smtp server
+  server.sendmail(USERNAME,USERNAME, message) # sending it to ourself
